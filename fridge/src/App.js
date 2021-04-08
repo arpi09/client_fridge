17b998a31b4dc120fd332a9558cf17acec70e9a9
Arvid <arvid.pilhall@me.com>
2021-04-08 22:43:58 +0200

    Fixed loadingprovider bug and added build

--- fridge/src/App.js
+++ fridge/src/App.js
@@ -3,29 +3,26 @@ import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import UserProvider from "./providers/UserProvider";
 import FridgeProvider from "./providers/FridgeProvider";
-import LoadingProvider from "./providers/LoadingProvider";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <LoadingProvider>
-      <UserProvider>
-        <Router>
-          <div style={{ height: "100vh" }}>
-            <Switch>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/">
-                <FridgeProvider>
-                  <Home />
-                </FridgeProvider>
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </UserProvider>
-    </LoadingProvider>
+    <UserProvider>
+      <Router>
+        <div style={{ height: "100vh" }}>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <FridgeProvider>
+                <Home />
+              </FridgeProvider>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </UserProvider>
   );
 }
 
